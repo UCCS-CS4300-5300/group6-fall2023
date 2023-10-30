95caa4c187e85ae3b3346bf1b757e6fd0fea785e
Nachiket Kamod <event_horizon@Nachikets-MacBook-Air.local>
2023-10-29 19:17:55 -0600

    Code cleanup and minor UI touches

--- popularity_assessor/views.py
+++ popularity_assessor/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request, user_name):
-    return render(request, 'profile.html')
+    return render(request, 'profile.html', {"count": [1,2,3]})
