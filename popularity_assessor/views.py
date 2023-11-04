0db4bf9a258dffbdec065413d262a5a2123e26bb
TonyRodriguez2001 <85913666+TonyRodriguez2001@users.noreply.github.com>
2023-11-04 18:47:57 +0000

    Added registration and login pages using djangos built in user model and user authentication.

--- popularity_assessor/views.py
+++ popularity_assessor/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def profile(request, user_name):
     return render(request, 'profile.html', {"count": [1,2,3]})
+
+def register(request):
+  # Handle the POST request (form submission)
+  if request.method == "POST":
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+          form.save()
+          # Optionally, you can log the user in after registration here
+          return redirect('login')  # Redirect to login page or some other page
+  # Handle the GET request (displaying the form)
+  else:
+      form = UserCreationForm()
+
+  context = {'form': form}
+  return render(request, 'register.html', context)
