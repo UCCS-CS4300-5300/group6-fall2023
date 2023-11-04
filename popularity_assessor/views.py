72a0476ffebd7f6d2d5677acee878a70c8443bab
ygutie0101 <113456370+ygutie0101@users.noreply.github.com>
2023-11-04 23:29:32 +0000

    Added a delete account and made validation for account deletion request

--- popularity_assessor/views.py
+++ popularity_assessor/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request, user_name):
-    return render(request, 'profile.html', {"count": [1,2,3]})
+  # For now, the only POST request is used to delete account.
+  # In the future, this must be checked further to very what the user want. (ex: delete vs. manage metric)
+  if request.method == "POST":
+    delete_account()
+    return render(request, "profile.html", {"count": [1,2,3], "confirm_delete_request": "Account deletion request received."})
+
+  return render(request, 'profile.html', {"count": [1,2,3]})
+
+def delete_account(user_id=None):
+  pass
+  
