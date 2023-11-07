e64a79389de42bacf75cd68d6967880b0de099d5
TonyRodriguez2001 <85913666+TonyRodriguez2001@users.noreply.github.com>
2023-11-07 21:16:17 +0000

    Fixed registration page form

---
+++ popularity_assessor/helpers.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.password_validation import password_validators_help_texts
+
+def get_password_validators_help_texts():
+    return password_validators_help_texts()
--- popularity_assessor/views.py
+++ popularity_assessor/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from .helpers import get_password_validators_help_texts
 
   # Create your views here.
 @login_required
@@ -9,21 +10,24 @@ def profile(request, user_name):
   return render(request, 'profile.html', {"count": [1,2,3]})
 
 def register(request):
-    # Handle the POST request (form submission)
   if request.method == "POST":
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      form.save()
-      # Log the user in after registration and redirect to profile
-      new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'])
-      login(request, new_user)
-      return redirect('popularity_assessor:profile', user_name=new_user.username)
-  # Handle the GET request (displaying the form)
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+          form.save()
+          # Log the user in after registration and redirect to profile
+          new_user = authenticate(username=form.cleaned_data['username'],
+                                  password=form.cleaned_data['password1'])
+          login(request, new_user)
+          return redirect('popularity_assessor:profile', user_name=new_user.username)
   else:
-    form = UserCreationForm()
+      form = UserCreationForm()
 
-  context = {'form': form}
+  # Include password help texts in context
+  password_help_texts = get_password_validators_help_texts()
+  context = {
+      'form': form,
+      'password_help_texts': password_help_texts
+  }
   return render(request, 'register.html', context)
 
 def custom_login(request):
