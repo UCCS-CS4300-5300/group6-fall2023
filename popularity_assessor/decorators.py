5e8d33039f7a7b6783d35f62a11a24665a9bc478
Nachiket Kamod <nkamod@uccs.edu>
2023-11-21 17:45:34 -0700

    Implemented redirect page if user is not connected with facebook

--- popularity_assessor/decorators.py
+++ popularity_assessor/decorators.py
@@ -29,34 +29,13 @@ def facebook_auth_check(view_func):
             validity = getAccessToken.debug(IGAcc.token, adminAccessToken)
 
             if validity != 'valid':
-                redirect("popularity_assessor:connect-insta")
+                request.message = "Looks like your Facebook access token has expired. Please reconnect your account."
+                redirect("popularity_assessor:connect-facebook")
 
             request.api = facebook_API(IGAcc.token, facebook_Config())
         except InstagramAccount.DoesNotExist:
-            # Generate a random number for CSRF protection
-            RANDOM_NUMBER = random.randrange(100000000, 999999999)
 
-            # Retrieve the Facebook client ID from the environment variable
-            # client_id = 1709283336164932 my testing one
-            client_id = os.getenv("FB_CLIENT_ID")
-            if client_id is None:
-                raise ValueError(
-                    "Facebook client id environment variable not set")
-            
-            url = request.build_absolute_uri(reverse('popularity_assessor:connect-insta'))
-            
-
-            fb_auth_url = f"https://www.facebook.com/v18.0/dialog/oauth?client_id={client_id}&redirect_uri={url}&response_type=code&state={RANDOM_NUMBER}"
-
-            # Generate the redirect URL for Facebook authentication
-            # params = {
-            #     "client_id": client_id,
-            #     "redirect_uri": request.build_absolute_uri(reverse("popularity_assessor:connect-insta")),
-            #     "response_type": "code",
-            #     "state": RANDOM_NUMBER
-            # }
-
-            return redirect(fb_auth_url)
+            return redirect("popularity_assessor:connect-facebook")
 
         # Continue to the original view
         return view_func(request, *args, **kwargs)
